bba0a33196e967a61f1e36c8b2eef28d839bbf16
Silas (Notebook) <santiagosilas@outlook.com>
2020-08-26 23:35:39 -0300

    Exemplo 2020-08-24

--- 2020-08-24/api/__init__.py
+++ 2020-08-24/api/__init__.py
@@ -1,7 +1,10 @@
 ﻿# coding: utf-8
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.debug = True
+app.config.from_object('config')
 
-from api import routes
+db = SQLAlchemy(app)
+
+from api import routes, models
